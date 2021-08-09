13bd5cc3d65b6b441a2393783671459f36c1a7e2
Igor Strozzi <istrozzi@matematica.ufrj.br>
2021-08-09 00:08:03 -0300

    cleaning

--- CuArrays/mnca.jl
+++ CuArrays/mnca.jl
@@ -1,123 +1,222 @@
-##
-using CUDA
+#%%
+using FFTW
 using LinearAlgebra
-using Dates
-using Images, TestImages, Colors
-using OffsetArrays
-using GLMakie
-using ImageFiltering
 using LazyGrids
-##
+using Random
+using GLMakie
+#%%
+const SIZE = 1 << 9
+const MID = SIZE ÷ 2
+const O = SIZE - MID
+Y, X = ndgrid(-O:(O-1), -O:(O-1))
 
+#%%
+orbium = Dict("name" => "Orbium","R" => 13,"T"=>10,"m"=>0.15,"s"=>0.015,"b"=>[1],
+  "cells" => [[0,0,0,0,0,0,0.1,0.14,0.1,0,0,0.03,0.03,0,0,0.3,0,0,0,0],
+  [0,0,0,0,0,0.08,0.24,0.3,0.3,0.18,0.14,0.15,0.16,0.15,0.09,0.2,0,0,0,0],
+  [0,0,0,0,0,0.15,0.34,0.44,0.46,0.38,0.18,0.14,0.11,0.13,0.19,0.18,0.45,0,0,0],
+  [0,0,0,0,0.06,0.13,0.39,0.5,0.5,0.37,0.06,0,0,0,0.02,0.16,0.68,0,0,0],
+  [0,0,0,0.11,0.17,0.17,0.33,0.4,0.38,0.28,0.14,0,0,0,0,0,0.18,0.42,0,0],
+  [0,0,0.09,0.18,0.13,0.06,0.08,0.26,0.32,0.32,0.27,0,0,0,0,0,0,0.82,0,0],
+  [0.27,0,0.16,0.12,0,0,0,0.25,0.38,0.44,0.45,0.34,0,0,0,0,0,0.22,0.17,0],
+  [0,0.07,0.2,0.02,0,0,0,0.31,0.48,0.57,0.6,0.57,0,0,0,0,0,0,0.49,0],
+  [0,0.59,0.19,0,0,0,0,0.2,0.57,0.69,0.76,0.76,0.49,0,0,0,0,0,0.36,0],
+  [0,0.58,0.19,0,0,0,0,0,0.67,0.83,0.9,0.92,0.87,0.12,0,0,0,0,0.22,0.07],
+  [0,0,0.46,0,0,0,0,0,0.7,0.93,1,1,1,0.61,0,0,0,0,0.18,0.11],
+  [0,0,0.82,0,0,0,0,0,0.47,1,1,0.98,1,0.96,0.27,0,0,0,0.19,0.1],
+  [0,0,0.46,0,0,0,0,0,0.25,1,1,0.84,0.92,0.97,0.54,0.14,0.04,0.1,0.21,0.05],
+  [0,0,0,0.4,0,0,0,0,0.09,0.8,1,0.82,0.8,0.85,0.63,0.31,0.18,0.19,0.2,0.01],
+  [0,0,0,0.36,0.1,0,0,0,0.05,0.54,0.86,0.79,0.74,0.72,0.6,0.39,0.28,0.24,0.13,0],
+  [0,0,0,0.01,0.3,0.07,0,0,0.08,0.36,0.64,0.7,0.64,0.6,0.51,0.39,0.29,0.19,0.04,0],
+  [0,0,0,0,0.1,0.24,0.14,0.1,0.15,0.29,0.45,0.53,0.52,0.46,0.4,0.31,0.21,0.08,0,0],
+  [0,0,0,0,0,0.08,0.21,0.21,0.22,0.29,0.36,0.39,0.37,0.33,0.26,0.18,0.09,0,0,0],
+  [0,0,0,0,0,0,0.03,0.13,0.19,0.22,0.24,0.24,0.23,0.18,0.13,0.05,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0.02,0.06,0.08,0.09,0.07,0.05,0.01,0,0,0,0,0]])
+#%%
+orbium["cells"] = hcat(orbium["cells"]...)
+#%%
+R = orbium["R"]
+m = orbium["m"]
+s = orbium["s"]
+b = orbium["b"]
+T = orbium["T"]
+
+#%%
+D(X, Y, R) = sqrt.((X/R) .^2 + (Y/R) .^2)
+bell(x, m, s) = exp(-((x-m)/s)^2 / 2)
+growth(U, m, s) = bell(U, m, s)*2-1
+F(x) = cos(SIZE*x)^3 + sin((SIZE ÷2)*x + π/2)
+
+# %%
 mutable struct MNCA
-    space::Matrix
-    kernels::Array
-    transition_functions::Array
-    states::Array
-end
+    A::Matrix{Float64}
+    R::Int64
+    μ::Float64
+    σ::Float64
+    β::Array{Float64}
+    dt::Float64
+    
+    S::Matrix{Float64}
+    K::Vector{Matrix{Float64}}
+    K_fft::Vector{Matrix{ComplexF64}}
+    
+    calc_kernels::Function
+    δ::Function
+    Φ::Vector{Function}
+    update!::Function
+    populate!::Function
 
+    U::Matrix{Float64}
+    G::Matrix{Float64}
 
-include("convolution.jl")
-using .Convolution
-##
-include("aux_funs.jl")
-using .Aux
-##
-CUDA.allowscalar(false)
-##
-function frames(configuration, niter, kernels, functions, states; params)
-    #params = Dict{String,Any}(["bin" => bin, "e" => e, "r" => r, "k" => k])
-    kdim, = size(ckern)
-    state_seq = [state]
-    for i = 1:niter
-        state = state_seq[end]
-        convolved = CUDA.zeros(n, n)
-        S = (state .>= bin) .* state # the spiking neurons
-        nS = (state .< bin) .* state # the complimentary matrix
-        spike = nS + (r * S)
-        conv(n, spike, ckern, convolved, kdim) # the spiking neuron have a 1.3fold greater influence over its neighbors
-        state = e * (nS + (k * S)) + (1 - e) * convolved # (nS + k*S) is the initial state but with the spiking neurons' states updated; (1-e)*conv is the influence the neighbors had over the neuron
-        # state = e * (( r * nS) + (k * S)) + (1 - e) * convolved # (nS + k*S) is the initial state but with the spiking neurons' states updated; (1-e)*conv is the influence the neighbors had over the neuron
-        push!(state_seq, deepcopy(state))
+    function MNCA(A, R, μ, σ, β, dt)
+
+        function calc_kernel(M::MNCA)
+            M.S = D(X, Y, R)
+            M.K = Vector{Matrix{Float64}}(undef, 0)
+            K1 = (M.S .< 1) .* bell.(M.S, 0.5, 0.15)
+            push!(M.K, K1)
+            K2 = clamp.((M.S .< 1) .* F.(bell.(M.S, 0.5, 0.15)), 0, 1)
+            push!(M.K, K2)
+            M.K_fft = Vector{Matrix{ComplexF64}}(undef, 0)
+            push!(M.K_fft, fft(fftshift(M.K[1]/sum(M.K[1]))))
+            push!(M.K_fft, fft(fftshift(M.K[2]/sum(M.K[2]))))
+        end
+
+        δ(U) = growth(U, μ, σ)
+
+        M = new(A, R, μ, σ, β, dt);
+        calc_kernel(M)
+
+        M.calc_kernels = calc_kernel
+        M.δ = δ
+
+        return M
     end
-    return state_seq, params
 end
-##
-const n = 256
-##
-setup_convolution(n)
-##
-niter = 300
-##
-Z2_region = [[x, y] for x in -128:1:127, y in 128:-1:-127]
-#%%
-transpose(Z2_region)
-
-#%%
-distances_field = norm.(Z2_region)
-
-##
-heatmap(distances_field)
-#%%
-F(X) = sin(X[1])^3 + cos(X[2] + π/2)
-
-f(x) = sin(x)^3 + cos(x + π/2)
-#%%
-trig_field = F.(Z2_region)
-#%%
-trig_field = f.(distances_field)
-
-#%%
-gaussian_kernel = Kernel.gaussian(3)
 #%%
 
 
-#%%
-gkernel = no_offset(gaussian_kernel)
-
-#%%
-#heatmap(gkernel)
-
-
-#%%
-#heatmap(trig_field)
-#%%
-kdim, = size(gkernel)
-convolved = CUDA.zeros(n, n)
-#%%
-device_field = cu(trig_field)
-device_kernel = cu(gkernel)
-
+A = zeros(SIZE, SIZE)
 #%%
 
 
-
-conv_field_gkern = conv(n, device_field, device_kernel, convolved, kdim)
-#%%
-conv(n, device_field, device_kernel, convolved, kdim)
-#%%
-host_conv = conv_field_gkern |> Array
-
+M = MNCA(A, R, m, s, b, 1/5)
 
 #%%
-conv
+function update1(M::MNCA)
+    M.U[:,:] = real(ifft(M.K_fft[1] .* fft(M.A)))[:,:]
+    M.G[:,:] = M.δ.(M.U)[:,:]
+    M.A[:, :] = clamp.(M.A + M.dt * M.G, 0, 1)[:, :]
+end
+function update2(M::MNCA)
+    M.U[:,:] = real(ifft(M.K_fft[2] .* fft(M.A)))[:,:]
+    M.G[:,:] = M.δ.(M.U)[:,:]
+    M.A[:, :] = clamp.(M.A + M.dt^2 * M.G, 0, 1)[:, :]
+end
+
+function update!(M::MNCA)
+    for ϕ in M.Φ
+        ϕ(M)
+    end
+end
+
+#%%
+M.Φ = Vector{Function}(undef, 0)
+push!(M.Φ, update1)
+push!(M.Φ, update2)
+M.update! = update!
+#%%
+function populate(W, creature_cells, num_creatures)
+    cx, cy = size(creature_cells)
+    to_populate = rand(cx:SIZE-cx, (num_creatures, 2))
+    for row in eachrow(to_populate)
+        x, y = row
+        W[(x-cx ÷ 2):(x+cx ÷ 2-1), (y-cx ÷ 2):(y+cx ÷ 2 -1)] = creature_cells
+    end
+end
+
+#%%
+M.A = A
+populate(M.A, orbium["cells"], 30)
+#%%
+M.populate! = () -> populate(M.A, orbium["cells"], 1)
 #%%
 
 
+function panels(M::MNCA)
 
-init_state = CUDA.rand(n, n)
-##
-@elapsed flist, params = frames(init_state, niter; ckern=ckern, r=r)
-#%%
-@elapsed host_outs = Array.(flist)
-##
+    M.U = real(ifft(M.K_fft[1] .* fft(M.A)))
+    U2 = real(ifft(M.K_fft[2] .* fft(M.A)))
+    M.G = M.δ.(M.U)
 
-push!(params, "a" => a)
-push!(params, "b" => b)
-push!(params, "kerpattern" => string(ckern_expr))
+    fig = Figure()
+
+    nA = Node(M.A)
+    heatmap(fig[1, 1], nA)
+
+    nU = Node(M.U)
+    heatmap(fig[1, 2], nU)
+
+    nG = Node(M.G)
+    heatmap(fig[2, 1], nG)
+
+    nU2 = Node(U2)
+    heatmap(fig[2, 2], U2)
+
+    fig, nA, nU, nG, nU2
+end
+
 
 #%%
-field = Node(host_outs[1])
-fig, hm = GLMakie.heatmap(field)
+fig, nA, nU, nG, nU2 = panels(M)
+
 #%%
-makie_record(fig, field, host_outs, params, niter, "spiking")
+fig
+#%%
+
+function run(M::MNCA)
+
+    #fig, nA, nU, nG = panels(A, M)
+
+    fps = 60
+    nframes = 360
+
+    for i = 1:nframes
+        M.Φ[1](M)
+        M.Φ[2](M)
+        nA[] = M.A
+        nU[] = M.U
+        nG[] = M.G
+        sleep(1/fps) # refreshes the display!
+    end
+end
+#%%
+#run(M)
+#%%
+
+function record_run(fig, M; nframes = 600, fps=36)
+    opath = pwd() * "/CuArrays/outputs/" * "lenia/"
+    mkpath(opath)
+    GLMakie.record(fig, opath * "mnorbia.mp4", 1:nframes; framerate = fps) do i
+        M.Φ[1](M)
+        M.Φ[2](M)
+        nA[] = M.A
+        nU[] = M.U
+        nG[] = M.G
+        sleep(1/fps) # refreshes the display!
+    end
+end
+
+#%%
+
+#record_run(fig, M)
+
+#%%
+
+include("sim_manager_min.jl")
+
+
+#%%
+
+simulate(M, fps=30)
